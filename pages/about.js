c9f1ac20de109dd994ed475cab7e6b9e1fcbde5e
Cody Durham <codydd@gmail.com>
2021-07-17 14:20:21 -0600

    Add HEAD with next/head to add title and metadata

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,19 @@
+import Head from 'next/head'
+
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
-        </div>
+        <>
+            <Head>
+            <title>Ninja List | About</title>
+            {/* the meta tag will help with SEO I think */}
+            <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+            </div>
+        </>
     ) 
 }
 export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+// this Head is used to show metadata. We can change the names so the names of the pages will show up in the tabs
 import Head from 'next/head'
 import Image from 'next/image'
 import Footer from '../components/Footer'
@@ -8,13 +9,21 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Hompage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        {/* the meta tag will help with SEO I think */}
+        <meta name="keywords" content="ninjas" />
+      </Head>
+
+      <div>
+        <h1 className={styles.title}>Hompage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   ) 
 }
