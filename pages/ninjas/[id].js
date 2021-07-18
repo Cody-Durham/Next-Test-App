1cd5c14703bded79321e8f689ff20f6463f7157a
Cody Durham <codydd@gmail.com>
2021-07-18 16:24:47 -0600

    Add new route params using [id]

---
+++ pages/ninjas/[id].js
@@ -0,0 +1,11 @@
+ // notes that the name of this file [id] is shown in brackets because NEXT will intrepret it as an id that 
+ // will change in the params.  -> [1] or [2] or [3] or [4] or [5] ect...
+
+ const Details = () => {
+     return (
+         <div>
+             <h1>Detals Page</h1>
+         </div>
+     ) 
+ }
+ export default Details
